1a705af91ded8e0ca2391d08bb4851ae3cae8d41
Andrei Avram <6795248+andreiavrammsd@users.noreply.github.com>
2024-09-01 03:11:53 +0300

    Ignore options

--- include/msd/zip.hpp
+++ include/msd/zip.hpp
@@ -114,13 +114,17 @@ class zip {
 
     value_type back()
     {
-        assert(!empty());  // LCOV_EXCL_LINE
+        // GCOVR_EXCL_START
+        assert(!empty());
+        // GCOVR_EXCL_STOP
         return *std::prev(begin() + size());
     }
 
     value_type back() const
     {
+        // LCOV_EXCL_START
         assert(!empty());  // LCOV_EXCL_LINE
+        // LCOV_EXCL_STOP
         return *std::prev(begin() + size());
     }
 
